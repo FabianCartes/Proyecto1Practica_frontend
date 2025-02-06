1c9bbfefbc01e2216fc30125e8a4e1cb0c25a167
Fabian Cartes <fabian.cartes04@gmail.com>
2025-02-06 18:15:39 -0300

    cambiando puerto

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Cambia el puerto si es necesario
+    port: process.env.DATABASE_URL || 3000, // Usa el puerto definido por Railway
+    host: true, // Asegura que el servidor acepte conexiones externas
   },
+  build: {
+    outDir: 'dist', // Asegura que los archivos se generen en dist
+    sourcemap: true, // Opcional: útil para depuración
+  },
+  preview: {
+    port: 3000, // Ajusta para coincidir con Railway
+    host: true, 
+  }
 });
